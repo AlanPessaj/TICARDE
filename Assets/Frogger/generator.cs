667c0ada33e8ef9d16f483f8fec04a83ec0e6642
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-08-06 11:58:12 -0300

    musho

---
+++ Assets/Frogger/generator.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour
+{
+    public GameObject[] secciones;
+    public GameObject pastito;
+    public GameObject camara;
+    public int distancia = 0;
+    public int dificultad = 1;
+    public int rangoDespawneo;
+    bool rapidGeneration = false;
+    public int difficultyScalar = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dificultad = (int)Mathf.Floor(distancia / difficultyScalar);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerarZonas();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            InvokeRepeating(nameof(GenerarZonas), 0, 0.5f);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            CancelInvoke();
+        }
+        if (rapidGeneration)
+        {
+            GenerarZonas();
+        }if (Input.GetKeyDown(KeyCode.K))
+        {
+            rapidGeneration = true;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            rapidGeneration = false;
+        }
+    }
+    void GenerarZonas()
+    {
+        SiguienteZona();
+        DespawnearZonas();
+    }
+    GameObject lastSeccion;
+    void SiguienteZona()
+    {
+        Instantiate(pastito, new Vector3(distancia, 0, 0), Quaternion.identity);
+        distancia++;
+        //camara.transform.position = new Vector3(camara.transform.position.x + 1, camara.transform.position.y, camara.transform.position.z);
+        GameObject seccion = secciones[Random.Range(0, secciones.Length)];
+        bool isRepated = false;
+        do
+        {
+            isRepated = seccion == lastSeccion;
+            if (!isRepated)
+            {
+                GenerarSeccion(seccion);
+                lastSeccion = seccion;
+            }
+            else
+            {
+                seccion = secciones[Random.Range(0, secciones.Length)];
+            }
+        } while (isRepated);
+    }
+
+    void GenerarSeccion(GameObject seccion)
+    {
+        int cantidad = Random.Range(dificultad, dificultad + 4);
+        for (int i = 0; i < cantidad; i++)
+        {
+            Instantiate(seccion, new Vector3(distancia, 0, 0), Quaternion.identity);
+            distancia++;
+            //camara.transform.position = new Vector3(camara.transform.position.x + 1, camara.transform.position.y, camara.transform.position.z);
+        }
+    }
+
+    void DespawnearZonas()
+    {
+        GameObject[] zonas = GameObject.FindGameObjectsWithTag("zona");
+        foreach (GameObject zona in zonas)
+        {
+            if (zona.transform.position.x < distancia - rangoDespawneo)
+            {
+                Destroy(zona);
+            }
+        }
+    }
+}
---
+++ Assets/Tenis/ballmover.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmover : MonoBehaviour
+{
+    public Transform puntoI;
+    public Transform puntoF;
+    public float altura;
+    public float stepSize = 0.1f;
+    float step = 0f;
+    public float vCOR;
+    public float hCOR;
+    public float minAltura;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool ignoredCollision = false;
+        if (altura < minAltura)
+        {
+            altura = 0f;
+            stepSize -= hCOR * Time.deltaTime;
+            stepSize = Mathf.Clamp(stepSize, 0f, Mathf.Infinity);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            puntoI.position = new Vector3(0, 1, 0);
+            puntoF.position = new Vector3(5, 1, 5);
+            stepSize = 3f;
+            altura = 2f;
+            step = 0f;
+        }
+        if (transform.position.y < 0)
+        {
+            ignoredCollision = true;
+        }
+        transform.position = new Vector3(Mathf.LerpUnclamped(puntoI.position.x, puntoF.position.x, step), puntoI.position.y, Mathf.LerpUnclamped(puntoI.position.z, puntoF.position.z, step));
+        step += stepSize * Time.deltaTime;
+        float x = Vector3.Distance(puntoI.position, new Vector3(transform.position.x, puntoI.position.y, transform.position.z));
+        float r1 = Vector3.Distance(puntoI.position, puntoF.position);
+        if (r1 != 0)
+        {
+            Vector2 intermedio = new Vector2(r1/2, altura - puntoI.position.y);
+            float a = intermedio.y/((intermedio.x-r1)*intermedio.x);
+            float pelotaY = a*(x-r1)*x;
+            transform.position = new Vector3(transform.position.x, pelotaY + puntoI.position.y, transform.position.z);
+        }
+        else
+        {
+            puntoI.position = new Vector3(0, 1, 0);
+            puntoF.position = new Vector3(5, 1, 5);
+            stepSize = 3f;
+            altura = 2f;
+            step = 0f;
+        }
+        if (ignoredCollision)
+        {
+            transform.position = new Vector3(transform.position.x, 20, transform.position.z);
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("piso"))
+        {
+            altura *= vCOR;
+            float distance = Mathf.Clamp(Vector3.Distance(puntoI.position, puntoF.position) - hCOR*(1+vCOR)*altura, 0f, Mathf.Infinity);
+            Vector3 direction = (puntoF.position - puntoI.position).normalized;
+            puntoI.position = new Vector3(transform.position.x, other.transform.position.y + 0.5f, transform.position.z);
+            Vector3 displacement = direction * distance;
+            puntoF.position = new Vector3(displacement.x + puntoI.position.x, other.transform.position.y + 0.5f, displacement.z + puntoI.position.z);
+        }
+        step = 0f;
+    }
+}
