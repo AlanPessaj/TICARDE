268608b36aef5eba8d6aa4d70081df6821c63a85
AlanPessaj <alanpessaj123@gmail.com>
2024-08-20 09:07:24 -0300

    update

--- Assets/FentisTennis/Scripts/PlayerController_FT.cs
+++ Assets/FentisTennis/Scripts/PlayerController_FT.cs
@@ -35,7 +35,22 @@ public class PlayerController_FT : MonoBehaviour
         }
         else
         {
-
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(new Vector3(-movementSpeed * 1 * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(new Vector3(0, 0, -movementSpeed * 1 * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(new Vector3(movementSpeed * 1 * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(new Vector3(0, 0, movementSpeed * 1 * Time.deltaTime));
+            }
         }
     }
 }
