82d01efbd001c2eb8f98cd709f34482af75f3f45
AlanPessaj <alanpessaj123@gmail.com>
2024-08-20 09:00:41 -0300

    playerController

--- Assets/FentisTennis/Scripts/PlayerController_FT.cs
+++ Assets/FentisTennis/Scripts/PlayerController_FT.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController_FT : MonoBehaviour
 {
+    public int movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,28 @@ public class PlayerController_FT : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameObject.name == "Player1")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3 (movementSpeed*1*Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(0, 0, movementSpeed * 1 * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(-movementSpeed * 1 * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(0, 0, -movementSpeed * 1 * Time.deltaTime));
+            }
+        }
+        else
+        {
+
+        }
     }
 }
