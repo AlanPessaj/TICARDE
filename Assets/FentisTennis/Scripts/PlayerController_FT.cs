77d06bdb04bf9f03f40ecb1a80616870affc8e4a
AlanPessaj <alanpessaj123@gmail.com>
2024-08-26 18:58:59 -0300

    Movimiento de saque terminado

--- Assets/FentisTennis/Scripts/PlayerController_FT.cs
+++ Assets/FentisTennis/Scripts/PlayerController_FT.cs
@@ -45,6 +45,22 @@ public class PlayerController_FT : MonoBehaviour
             }
             else if(gamemanager.serve == 2)
             {
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.Translate(new Vector3(-movementSpeed * 1 * Time.deltaTime, 0, 0));
+                }
+                if (Input.GetKey(KeyCode.A) && transform.position.z < 0)
+                {
+                    transform.Translate(new Vector3(0, 0, (movementSpeed * Time.deltaTime)));
+                }
+                if (Input.GetKey(KeyCode.W) && transform.position.x < -30)
+                {
+                    transform.Translate(new Vector3((movementSpeed * Time.deltaTime), 0, 0));
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Translate(new Vector3(0, 0, -movementSpeed * 1 * Time.deltaTime));
+                }
 
             }
             else if (gamemanager.serve == 0)
@@ -98,50 +114,96 @@ public class PlayerController_FT : MonoBehaviour
         }
         else
         {
-            if (Input.GetKey(KeyCode.UpArrow))
+            if(gamemanager.serve == 2)
             {
-                transform.Translate(new Vector3(movementSpeed * 1 * Time.deltaTime, 0, 0));
+                if (Input.GetKey(KeyCode.UpArrow) && transform.position.x < 50)
+                {
+                    transform.Translate(new Vector3((movementSpeed * Time.deltaTime)/4, 0, 0));
+                }
+                if (Input.GetKey(KeyCode.LeftArrow) && transform.position.z < 30)
+                {
+                    transform.Translate(new Vector3(0, 0, (movementSpeed * Time.deltaTime)/4));
+                }
+                if (Input.GetKey(KeyCode.DownArrow) && transform.position.x > 45)
+                {
+                    transform.Translate(new Vector3((-movementSpeed * Time.deltaTime)/4, 0, 0));
+                }
+                if (Input.GetKey(KeyCode.RightArrow) && transform.position.z > 0)
+                {
+                    transform.Translate(new Vector3(0, 0, (-movementSpeed * Time.deltaTime)/4));
+                }
+                if (Input.GetButtonDown("A2"))
+                {
+                    racket.transform.Rotate(-90, 0, 0);
+                }
             }
-            if (Input.GetKey(KeyCode.LeftArrow))
+            else if (gamemanager.serve == 1)
             {
-                transform.Translate(new Vector3(0, 0, movementSpeed * 1 * Time.deltaTime));
+                /*if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    transform.Translate(new Vector3(movementSpeed * 1 * Time.deltaTime, 0, 0));
+                }
+                if (Input.GetKey(KeyCode.LeftArrow) && transform.position.z > 0)
+                {
+                    transform.Translate(new Vector3(0, 0, (-movementSpeed * Time.deltaTime)));
+                }
+                if (Input.GetKey(KeyCode.UpArrow) && transform.position.x < 30)
+                {
+                    transform.Translate(new Vector3((-movementSpeed * Time.deltaTime), 0, 0));
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(new Vector3(0, 0, movementSpeed * 1 * Time.deltaTime));
+                }*/
             }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Translate(new Vector3(-movementSpeed * 1 * Time.deltaTime, 0, 0));
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(new Vector3(0, 0, -movementSpeed * 1 * Time.deltaTime));
-            }
-            if (Input.GetButtonDown("A2"))
-            {
-                racket.transform.Rotate(-90, 0, 0);
-            }
-            if (Input.GetButton("A2"))
+            else if(gamemanager.serve == 0)
             {
 
-                timer += Time.deltaTime;
-                if (chargingShot)
+                if (Input.GetKey(KeyCode.UpArrow))
                 {
-                    rotation += Time.deltaTime * racketSpeed / 2;
-                    racketPivot.transform.rotation = Quaternion.Euler(0, Mathf.Lerp(180, 225, rotation), 0);
-                    chargingShot = rotation <= 1;
-                    if (!chargingShot)
-                        rotation = 0;
+                    transform.Translate(new Vector3(movementSpeed * 1 * Time.deltaTime, 0, 0));
                 }
-                else
+                if (Input.GetKey(KeyCode.LeftArrow))
                 {
-                    rotation += Time.deltaTime * racketSpeed;
-                    racketPivot.transform.rotation = Quaternion.Euler(0, Mathf.Lerp(225, 90, rotation), 0);
+                    transform.Translate(new Vector3(0, 0, movementSpeed * 1 * Time.deltaTime));
                 }
-            }
-            if (Input.GetButtonUp("A2"))
-            {
-                racket.transform.Rotate(90, 0, 0);
-                racketPivot.transform.rotation = Quaternion.Euler(0, 180, 0);
-                rotation = 0;
-                chargingShot = true;
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    transform.Translate(new Vector3(-movementSpeed * 1 * Time.deltaTime, 0, 0));
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(new Vector3(0, 0, -movementSpeed * 1 * Time.deltaTime));
+                }
+                if (Input.GetButtonDown("A2"))
+                {
+                    racket.transform.Rotate(-90, 0, 0);
+                }
+                if (Input.GetButton("A2"))
+                {
+
+                    timer += Time.deltaTime;
+                    if (chargingShot)
+                    {
+                        rotation += Time.deltaTime * racketSpeed / 2;
+                        racketPivot.transform.rotation = Quaternion.Euler(0, Mathf.Lerp(180, 225, rotation), 0);
+                        chargingShot = rotation <= 1;
+                        if (!chargingShot)
+                            rotation = 0;
+                    }
+                    else
+                    {
+                        rotation += Time.deltaTime * racketSpeed;
+                        racketPivot.transform.rotation = Quaternion.Euler(0, Mathf.Lerp(225, 90, rotation), 0);
+                    }
+                }
+                if (Input.GetButtonUp("A2"))
+                {
+                    racket.transform.Rotate(90, 0, 0);
+                    racketPivot.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    rotation = 0;
+                    chargingShot = true;
+                }
             }
         }
     }
