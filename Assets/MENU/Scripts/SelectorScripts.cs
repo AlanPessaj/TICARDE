ba51860afe8aff02fe7bcf8896aeac001fd2e3b3
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-09-09 15:10:41 -0300

    commit de emergencia

---
+++ Assets/MENU/Scripts/SelectorScripts.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorScripts : MonoBehaviour
+{
+    public GameObject[] logos;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (index > 0)
+            {
+                index--;
+            }
+            else
+            {
+                index = logos.Length - 1;
+            }
+            foreach (var item in logos)
+            {
+                item.SetActive(false);
+            }
+            logos[index].SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (index < logos.Length - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+            foreach (var item in logos)
+            {
+                item.SetActive(false);
+            }
+            logos[index].SetActive(true);
+        }
+    }
+}
