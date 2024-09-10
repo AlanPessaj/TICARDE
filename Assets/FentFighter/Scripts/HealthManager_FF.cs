84bdac58bdc92f5b863d78568448d7416549a2e4
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-09-10 10:13:58 -0300

    baara the bida

---
+++ Assets/FentFighter/Scripts/HealthManager_FF.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager_FF : MonoBehaviour
+{
+    public GameObject UI;
+    public float health;
+    public float maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            UpdateHealth();
+        }
+    }
+
+    void UpdateHealth()
+    {
+        UI.transform.GetChild(0).localPosition = new Vector3(Mathf.Lerp(385, 0, Mathf.InverseLerp(0, maxHealth, health)), 0, 0);
+    }
+}
