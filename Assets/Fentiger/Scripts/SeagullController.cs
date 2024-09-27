da90300234a8c8f4826062eb7b7eee6e7b657c96
AlanPessaj <alanpessaj123@gmail.com>
2024-09-27 10:41:20 -0300

    gaviota master B)

---
+++ Assets/Fentiger/Scripts/SeagullController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeagullController : MonoBehaviour
+{
+    bool firstTime;
+    public GameObject[] players = new GameObject[2];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (firstTime && (players[0].transform.position.x == transform.position.x-3 || players[1].transform.position.x == transform.position.x - 3))
+        {
+            //Spawnear
+        }
+    }
+}
