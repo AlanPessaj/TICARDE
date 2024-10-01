7205647f1524918072517e42e71e00262fb87706
AlanPessaj <alanpessaj123@gmail.com>
2024-10-01 12:06:26 -0300

    navmesh terminada y lion

---
+++ Assets/Fentiger/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    GameObject[] players = new GameObject[2];
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        players[0] = GameObject.Find("Player1");
+        players[1] = GameObject.Find("Player2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(players[0].transform.position, transform.position) < Vector3.Distance(players[1].transform.position, transform.position))
+        {
+            agent.destination = players[0].transform.position;
+        }
+        else
+        {
+            agent.destination = players[1].transform.position;
+        }
+    }
+}
