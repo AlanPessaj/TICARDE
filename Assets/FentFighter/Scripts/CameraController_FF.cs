56828eb6829c53d1995dda18c767618b8b89e239
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-10-01 14:26:43 -0300

    no c

---
+++ Assets/FentFighter/Scripts/CameraController_FF.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController_FF : MonoBehaviour
+{
+    public Transform[] players;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3((players[0].position.x + players[1].position.x) / 2, 2.3f, -10.4f);
+    }
+}
