68facec0ec975306b0154ad984ba2ab2b8b7ed70
AlanPessaj <alanpessaj123@gmail.com>
2024-10-02 11:46:07 -0300

    low batery, please charge

---
+++ Assets/Fentiger/Scripts/FrogController_FG.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController_FG : MonoBehaviour
+{
+    // Movimiento de 2 a 3 tiles por direccion cardinal
+    GameObject[] players = new GameObject[2];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        players[0] = GameObject.Find("Player1");
+        players[1] = GameObject.Find("Player2");
+    }
+}
