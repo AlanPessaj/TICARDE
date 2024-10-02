941f17e76824c3521cc718e620dd6e0c7969ac1f
AlanPessaj <alanpessaj123@gmail.com>
2024-10-02 19:43:10 -0300

    ranarda salta :)

--- Assets/Fentiger/Scripts/FrogController_FG.cs
+++ Assets/Fentiger/Scripts/FrogController_FG.cs
@@ -4,64 +4,94 @@ using UnityEngine;
 
 public class FrogController_FG : MonoBehaviour
 {
-    // Movimiento de 2 a 3 tiles por direccion cardinal
     GameObject[] players = new GameObject[2];
     int distance;
     int direction;
-    Vector3 pos;
+    Vector3 startPos;
+    Vector3 targetPos;
     public bool takenSpot;
-    void Start()
+    public float jumpHeight = 2f;
+    public float jumpSpeed = 2f;
+    public float jumpDelay = 1f;
+    private float jumpProgress = 0f;
+    private float delayTimer = 0f;
+    private bool isJumping = false;
+    public GameObject checker;
+
+    private void Start()
     {
-
+        checker.transform.parent = null;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        do
+        if (!isJumping)
         {
-            distance = Random.Range(2, 4);
-            direction = Random.Range(0, 4);
-            switch (direction)
+            delayTimer += Time.deltaTime;
+            if (delayTimer >= jumpDelay)
             {
-                case 0:
-                    pos = transform.forward * distance;
-                    break;
-                case 1:
-                    pos = transform.right * distance;
-                    break;
-                case 2:
-                    pos = transform.forward * -distance;
-                    break;
-                case 3:
-                    pos = transform.right * -distance;
-                    break;
+                do
+                {
+                    distance = Random.Range(2, 4);
+                    direction = Random.Range(0, 4);
+                    switch (direction)
+                    {
+                        case 0:
+                            targetPos = transform.position + transform.forward * distance;
+                            break;
+                        case 1:
+                            targetPos = transform.position + transform.right * distance;
+                            break;
+                        case 2:
+                            targetPos = transform.position + transform.forward * -distance;
+                            break;
+                        case 3:
+                            targetPos = transform.position + transform.right * -distance;
+                            break;
+                    }
+                }
+                while (!FreeSpot(targetPos));
+
+                startPos = transform.position;
+                isJumping = true;
+                jumpProgress = 0f;
+                delayTimer = 0f;
             }
         }
-        while (!FreeSpot(pos));
+        else
+        {
+            JumpToPosition(targetPos);
+        }
+    }
 
-        //Saltar hacia la direccion
+    void JumpToPosition(Vector3 targetPosition)
+    {
+        jumpProgress += Time.deltaTime * jumpSpeed;
+        Vector3 horizontalPosition = Vector3.Lerp(startPos, targetPosition, jumpProgress);
+        float arc = Mathf.Sin(Mathf.PI * jumpProgress) * jumpHeight;
+        Vector3 finalPosition = new Vector3(horizontalPosition.x, horizontalPosition.y + arc, horizontalPosition.z);
+        transform.position = finalPosition;
+        if (jumpProgress >= 1f)
+        {
+            isJumping = false;
+        }
     }
 
     bool FreeSpot(Vector3 pos)
     {
-        //Mover trigger a pos
-        // devolver si hay algo
-        transform.GetChild(1).localPosition = pos;
+        checker.transform.position = pos;
         if (!takenSpot)
         {
             return true;
         }
         else
         {
-            transform.GetChild(1).localPosition = new Vector3();
             return false;
         }
     }
+
     private void Awake()
     {
         players[0] = GameObject.Find("Player1");
         players[1] = GameObject.Find("Player2");
     }
-
 }
