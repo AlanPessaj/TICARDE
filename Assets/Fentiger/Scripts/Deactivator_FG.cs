4c1c95ed2cf6856bbcd8e38912b0c75d06ce5626
AlanPessaj <alanpessaj123@gmail.com>
2024-10-21 14:57:40 -0300

    batery

--- Assets/Fentiger/Scripts/Deactivator_FG.cs
+++ Assets/Fentiger/Scripts/Deactivator_FG.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Deactivator_FG : MonoBehaviour
 {
@@ -9,12 +10,19 @@ public class Deactivator_FG : MonoBehaviour
         if (GameObject.Find("Player1") == null && GameObject.Find("Player2") == null)
         {
             MonoBehaviour[] allScripts = FindObjectsOfType<MonoBehaviour>();
+            NavMeshAgent[] allAgents = FindObjectsOfType<NavMeshAgent>();
+            
             foreach (MonoBehaviour script in allScripts)
             {
                 if (script != this && !(script is DieScript_FG) && !(script is Generator_FG))
                 {
                     script.enabled = false;
                 }
+            }
+
+            foreach (NavMeshAgent item in allAgents)
+            {
+                
             }
         }
     }
