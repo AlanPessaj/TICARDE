eb9147e64f08ac9defe24310e74c1b0cc7436dd9
AlanPessaj <alanpessaj123@gmail.com>
2024-10-23 13:17:23 -0300

    seguir con el child de camara para ovni

---
+++ Assets/Fentiger/Scripts/OvniController_FG.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvniController_FG : MonoBehaviour
+{
+    public float orbitSpeed = 50f;
+    public float orbitDuration = 3f;
+    private float elapsedTime = 0f;
+    private bool isOrbiting = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isOrbiting)
+        {
+            transform.RotateAround(transform.parent.position, Vector3.up, orbitSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= orbitDuration)
+            {
+                isOrbiting = false;
+            }
+        }
+    }
+}
