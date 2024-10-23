88535a38b0bd72163ec89d744e9c653cf5c0a06a
AlanPessaj <alanpessaj123@gmail.com>
2024-10-23 02:05:13 -0300

    pickable hearts

---
+++ Assets/Fentiger/Scripts/HeartController_FG.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartController_FG : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collision.transform.GetChild(0).gameObject.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
