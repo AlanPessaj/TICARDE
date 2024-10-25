e55939f920264eb0a738626223b231437f65df2c
AlanPessaj <alanpessaj123@gmail.com>
2024-10-25 15:14:06 -0300

    LASER TERMINADO

--- Assets/Fentiger/Scripts/LaserScaler_FG.cs
+++ Assets/Fentiger/Scripts/LaserScaler_FG.cs
@@ -14,12 +14,12 @@ public class LaserScaler_FG : MonoBehaviour
         {
             toDivide = 24 / hit.distance * 2;
             transform.position = transform.GetChild(0).position + transform.up * (hit.distance / 2f);
-            transform.localScale = new Vector3(0.2f, 24 / toDivide, 0.2f);
+            transform.localScale = new Vector3(0.1f, 24 / toDivide, 0.1f);
         }
         else
         {
             transform.localPosition = new Vector3(-14, -18, -5);
-            transform.localScale = new Vector3(0.2f, 24, 0.2f);
+            transform.localScale = new Vector3(0.1f, 24, 0.1f);
         }
     }
 
