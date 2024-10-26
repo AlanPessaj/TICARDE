9088498d18b7d1fa0ccffa22acadf088e96dfd1b
AlanPessaj <alanpessaj123@gmail.com>
2024-10-25 23:15:55 -0300

    funcionalidad portales check

--- Assets/Fentiger/Scripts/Glad0s_FG.cs
+++ Assets/Fentiger/Scripts/Glad0s_FG.cs
@@ -6,9 +6,26 @@ public class Glad0s_FG : MonoBehaviour
 {
     public GameObject redPrefab;
     public GameObject redPortal;
+    int randomX;
+    int randomZ;
     // Start is called before the first frame update
     void Start()
     {
-        redPortal = Instantiate(redPrefab, new Vector3(transform.position.x + Random.Range(30, 71), transform.position.y, Random.Range(-12, 12)), Quaternion.Euler(0,0,90));
+        randomX = Random.Range(30, 71);
+        int tries = 1;
+        bool foundGrass = false;
+
+        while (!foundGrass)
+        {
+            if (Physics.Raycast(new Vector3(randomX, transform.position.y, 0) + new Vector3(tries, 0, 0), Vector3.down, out RaycastHit hit, 10f, LayerMask.GetMask("Grass")))
+            {
+                if (hit.collider != null)
+                {
+                    foundGrass = true;
+                    redPortal = Instantiate(redPrefab, new Vector3(tries + randomX, transform.position.y, Random.Range(-12, 12)), Quaternion.Euler(0, 0, 90));
+                }
+            }
+            tries++;
+        }
     }
 }
