a41856ea37b34767b3998c4e349dd5ada42ccb1f
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-10-25 22:39:38 -0300

    mejor animacion de leave

--- Assets/Fentiger/Scripts/OvniController_FG.cs
+++ Assets/Fentiger/Scripts/OvniController_FG.cs
@@ -8,42 +8,44 @@ public class OvniController_FG : MonoBehaviour
     bool hasFinishedOrbiting = false;
     bool oscillate = false;
     bool leave = false;
-    bool gotInPos = false;
     public float orbitSpeed;
     public float orbitDuration;
     float elapsedTime = 0f;
-    public float movingDelay;
-    float movingTime = 0f;
-    float t = 0f;
-    public float oscillateSpeed = 2f;
+    public float movingTime = 0f;
+    float oscillateTime = 0f;
+    public float oscillateSpeed;
     public Material yellow;
     public Material red;
     GameObject laser;
     bool firstTime = true;
     float leaveTime = 0f;
-    Vector3 leaveRotation;
+    float leaveTargetTime;
     Vector3 leavePos;
+    Quaternion leaveRot;
+    Vector3 transitionPos;
+    Quaternion transitionRot;
 
     private void Update()
     {
         if (isOrbiting)
         {
-            transform.RotateAround(transform.parent.position, Vector3.up, orbitSpeed * Time.deltaTime);
             elapsedTime += Time.deltaTime;
+            transform.RotateAround(transform.parent.position, Vector3.up, orbitSpeed * Time.deltaTime);
             if (elapsedTime >= orbitDuration)
             {
                 isOrbiting = false;
                 hasFinishedOrbiting = true;
+                transitionPos = transform.localPosition;
+                transitionRot = transform.rotation;
             }
         }
 
         if (!isOrbiting && hasFinishedOrbiting)
         {
             movingTime += Time.deltaTime;
-            float t = Mathf.Clamp01(movingTime / movingDelay);
-            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(11.4f, 6.15f, 9f), t);
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(-1.9f, 102.2f, 308f)), t);
-            if (t >= 0.1f)
+            transform.localPosition = Vector3.Lerp(transitionPos, new Vector3(11.4f, 6.15f, 9f), movingTime);
+            transform.rotation = Quaternion.Lerp(transitionRot, Quaternion.Euler(new Vector3(-1.9f, 102.2f, 308f)), movingTime);
+            if (movingTime >= 1f)
             {
                 hasFinishedOrbiting = false;
                 oscillate = true;
@@ -52,8 +54,8 @@ public class OvniController_FG : MonoBehaviour
 
         if (oscillate)
         {
-            t += Time.deltaTime * oscillateSpeed;
-            transform.localPosition = Vector3.Lerp(new Vector3(11.4f, 6.15f, 9f), new Vector3(8.5f, -6.61f, 4.84f), Mathf.PingPong(t, 1f));
+            oscillateTime += Time.deltaTime * oscillateSpeed;
+            transform.localPosition = Vector3.Lerp(new Vector3(11.4f, 6.15f, 9f), new Vector3(8.5f, -6.61f, 4.84f), Mathf.PingPong(oscillateTime, 1f));
             if (firstTime)
             {
                 StartCoroutine(Laser());
@@ -63,28 +65,16 @@ public class OvniController_FG : MonoBehaviour
 
         if (leave)
         {
-            if (!gotInPos)
+            leaveTargetTime += Time.deltaTime;
+            leaveTime += Time.deltaTime / 5;
+            Vector3 leaveTarget = Vector3.Lerp(new Vector3(0, 3, 4), new Vector3(0, 1, -8), leaveTargetTime);
+            transform.rotation = Quaternion.Lerp(leaveRot, Quaternion.identity, leaveTargetTime);
+            transform.localPosition = Vector3.Lerp(leavePos, leaveTarget, leaveTime);
+            leavePos = transform.localPosition;
+            if (leaveTargetTime >= 1)
             {
-                leaveTime += Time.deltaTime;
-                transform.localPosition = Vector3.Lerp(leavePos, new Vector3(0, 3, 4), leaveTime);
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), leaveTime);
-                if (leaveTime >= 1)
-                {
-                    gotInPos = true;
-                    leaveTime = 0;
-                    leavePos = transform.localPosition;
-                }
+                Destroy(gameObject);
             }
-            else
-            {
-                leaveTime += Time.deltaTime;
-                transform.localPosition = Vector3.Lerp(leavePos, new Vector3(0, 1, -8), leaveTime);
-                if (leaveTime >= 1)
-                {
-                    Destroy(gameObject);
-                }
-            }
-            // 1 -8
         }
     }
 
@@ -102,6 +92,6 @@ public class OvniController_FG : MonoBehaviour
         laser.SetActive(false);
         leave = true;
         leavePos = transform.localPosition;
-        leaveRotation = transform.rotation.eulerAngles;
+        leaveRot = transform.rotation;
     }
 }
