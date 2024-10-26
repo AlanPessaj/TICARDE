937060f6708679f20503b0532dd6e1f0c059c4b2
AlanPessaj <alanpessaj123@gmail.com>
2024-10-25 21:52:50 -0300

    glados :)

---
+++ Assets/Fentiger/Scripts/Glad0s_FG.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glad0s_FG : MonoBehaviour
+{
+    public GameObject redPrefab;
+    public GameObject redPortal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        redPortal = Instantiate(redPrefab, new Vector3(transform.position.x + Random.Range(30, 71), transform.position.y, Random.Range(-12, 12)), Quaternion.Euler(0,0,90));
+    }
+}
