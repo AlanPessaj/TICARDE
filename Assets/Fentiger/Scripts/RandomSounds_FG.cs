2d4e6776de904a83655b00847bdd74a66ac964dd
AlanPessaj <alanpessaj123@gmail.com>
2024-10-27 18:49:51 -0300

    honk sfx

---
+++ Assets/Fentiger/Scripts/RandomSounds_FG.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSounds_FG : MonoBehaviour
+{
+    public AudioClip[] sounds;
+
+    private void Start()
+    {
+        InvokeRepeating("PlaySound", 0, 1);
+    }
+
+    void PlaySound()
+    {
+        if (Random.Range(1, 101) <= 1 && Time.timeScale == 1)
+        {
+            GetComponent<AudioSource>().PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+        }
+    }
+}
