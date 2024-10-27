bf3ac3878da3da3b74092e08d5b30f12684bf49f
AlanPessaj <alanpessaj123@gmail.com>
2024-10-27 04:10:10 -0300

    AMIGO SOY UN GENIO
    NO PUEDO CREER QUE FUNCIONO A LA PRIMERA, CREO QUE TODOS DEBERIAN PONERSE EN FILA PARA CHUPARME TODOS LA PIJA, NO PUEDO CREER, SOY RE INTELIGENTE AMIGO, ESTO LO VOY A PONER EN EL CV SEGURISIMO, NO HAY CHANCE.

---
+++ Assets/Fentiger/Scripts/SoundManager_FG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager_FG : MonoBehaviour
+{
+    public Generator_FG generator;
+    public AudioClip[] mainTheme;
+    int level = 0;
+    void Update()
+    {
+        if (level != generator.Level)
+        {
+            level = generator.Level;
+            float time = GetComponent<AudioSource>().time;
+            float duration = GetComponent<AudioSource>().clip.length;
+            GetComponent<AudioSource>().clip = mainTheme[level];
+            GetComponent<AudioSource>().time = Mathf.Lerp(0, GetComponent<AudioSource>().clip.length, Mathf.InverseLerp(0, duration, time));
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
