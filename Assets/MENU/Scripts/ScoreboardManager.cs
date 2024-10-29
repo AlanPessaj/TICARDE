eb60ea9db3e32c8a9e088b6e00adde9946e51ade
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-10-29 12:06:14 -0300

    leaderboard hecha

---
+++ Assets/MENU/Scripts/ScoreboardManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreboardManager : MonoBehaviour
+{
+    public Text[] names;
+    public Text[] scores;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void LoadScores(string game)
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            names[i].text = PlayerPrefs.GetString($"{game}/name{i}", "NONE");
+        }
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i].text = PlayerPrefs.GetInt($"{game}/score{i}", 0).ToString();
+        }
+    }
+}
