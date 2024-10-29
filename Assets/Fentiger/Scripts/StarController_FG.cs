00752b55d9015dc30842a3084d30cf79893ad96b
AlanPessaj <alanpessaj123@gmail.com>
2024-10-29 11:34:31 -0300

    sonido star

--- Assets/Fentiger/Scripts/StarController_FG.cs
+++ Assets/Fentiger/Scripts/StarController_FG.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class StarController_FG : MonoBehaviour
 {
+    public AudioClip pickUp;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             if (collision.transform.GetComponent<UIManager_FG>().AddXP(25))
             {
-                //SONIDO
+                collision.transform.GetComponent<PlayerController_FG>().generator.GetComponent<SoundManager_FG>().PlaySound(pickUp);
                 Destroy(gameObject);
             }
         }
