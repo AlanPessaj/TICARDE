96dfa921f82325772e39e90089809b5b101e306e
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-10-29 08:52:12 -0300

    añadi xp

---
+++ Assets/Fentiger/Scripts/UIManager_FG.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager_FG : MonoBehaviour
+{
+    public GameObject UI;
+    public float maxXP;
+    public float XP;
+
+    public void AddXP(float value)
+    {
+        XP = Mathf.Clamp(XP + value, 0, maxXP);
+        UI.transform.GetChild(1).localPosition = new Vector3(Mathf.Lerp(385, 0, Mathf.InverseLerp(0, maxXP, XP)), -38.4f, 0);
+    }
+    public bool RemoveXP(float value)
+    {
+        if (value <= XP)
+        {
+            XP = Mathf.Clamp(XP - value, 0, maxXP);
+            UI.transform.GetChild(1).localPosition = new Vector3(Mathf.Lerp(385, 0, Mathf.InverseLerp(0, maxXP, XP)), -38.4f, 0);
+            return true;
+        }
+        return false;
+    }
+}
