930512a794fe7cdc477cbf7bdd1157df28ba5dac
AlanPessaj <alanpessaj123@gmail.com>
2024-10-30 12:05:12 -0300

    movi el panel :de: :D

--- Assets/MENU/Scripts/NameInputManager.cs
+++ Assets/MENU/Scripts/NameInputManager.cs
@@ -5,25 +5,22 @@ using UnityEngine.UI;
 
 public class NameInputManager : MonoBehaviour
 {
+    public RectTransform player1;
+    public RectTransform player2;
     public bool multiplayer;
-    public string player1Name;
-    public string player2Name;
-    public GameObject player1;
-    public GameObject player2;
-    // Start is called before the first frame update
+
     void Update()
     {
-        Debug.Log(player1.GetComponent<RectTransform>().anchoredPosition.ToString() + player2.GetComponent<RectTransform>().anchoredPosition.ToString());
         if (multiplayer)
         {
-            player1.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -70);
-            player2.SetActive(true);
-            player1.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -70);
+            player1.anchoredPosition = new Vector2(-280, 0);
+            player2.anchoredPosition = new Vector2(280, 0);
+            player2.gameObject.SetActive(true);
         }
         else
         {
-            player2.SetActive(false);
-            player1.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 70);
+            player1.anchoredPosition = new Vector2(0, 0);
+            player2.gameObject.SetActive(false);
         }
     }
 }
