a17e6141e525b5fffb2110ecdba83ed285660273
AlanPessaj <alanpessaj123@gmail.com>
2024-10-31 19:05:38 -0300

    checkpoint

--- Assets/MENU/Scripts/NameController.cs
+++ Assets/MENU/Scripts/NameController.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class NameController : MonoBehaviour
 {
+    public NameInputManager manager;
     public RectTransform[] chars = new RectTransform[4];
     char[] characters = new char[36];
     int index = 0;
     Coroutine instance;
     int selectedChar = 0;
+    public bool finished = false;
 
     private void Start()
     {
@@ -17,7 +18,6 @@ public class NameController : MonoBehaviour
         {
             characters[i] = (char)('A' + i);
         }
-
         for (int i = 0; i < 10; i++)
         {
             characters[26 + i] = (char)('0' + i);
@@ -31,81 +31,114 @@ public class NameController : MonoBehaviour
 
         if (gameObject.name == "Player1")
         {
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetButtonDown("A") && manager.instance == null)
             {
-                index++;
-                if (index >= chars.Length) index = 0;
-                childRect.anchoredPosition = chars[index].anchoredPosition;
-                selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
-            }
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                index--;
-                if (index < 0) index = chars.Length - 1;
-                childRect.anchoredPosition = chars[index].anchoredPosition;
-                selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
-            }
-
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                selectedChar = (selectedChar - 1 + characters.Length) % characters.Length;
-                chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
-                if (instance == null)
+                finished = !finished;
+                Color targetColor = finished ? Color.green : Color.white;
+                foreach (RectTransform caracter in chars)
                 {
-                    instance = StartCoroutine(Move(0));
+                    caracter.GetComponent<TextMeshProUGUI>().color = targetColor;
                 }
             }
 
-            if (Input.GetKeyDown(KeyCode.S))
+            if (!finished)
             {
-                selectedChar = (selectedChar + 1) % characters.Length;
-                chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
-                if (instance == null)
+                transform.GetChild(0).gameObject.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.D))
                 {
-                    instance = StartCoroutine(Move(1));
+                    index = (index + 1) % chars.Length;
+                    childRect.anchoredPosition = chars[index].anchoredPosition;
+                    selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
                 }
+
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    index = (index - 1 + chars.Length) % chars.Length;
+                    childRect.anchoredPosition = chars[index].anchoredPosition;
+                    selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
+                }
+
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    selectedChar = (selectedChar - 1 + characters.Length) % characters.Length;
+                    chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
+                    if (instance == null)
+                    {
+                        instance = StartCoroutine(Move(0));
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    selectedChar = (selectedChar + 1) % characters.Length;
+                    chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
+                    if (instance == null)
+                    {
+                        instance = StartCoroutine(Move(1));
+                    }
+                }
+            }
+            else
+            {
+                transform.GetChild(0).gameObject.SetActive(false);
             }
         }
-        else
+        else if (gameObject.name == "Player2")
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetButtonDown("A2") && manager.instance == null)
             {
-                index++;
-                if (index >= chars.Length) index = 0;
-                childRect.anchoredPosition = chars[index].anchoredPosition;
-                selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                index--;
-                if (index < 0) index = chars.Length - 1;
-                childRect.anchoredPosition = chars[index].anchoredPosition;
-                selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
-            }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                selectedChar = (selectedChar - 1 + characters.Length) % characters.Length;
-                chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
-                if (instance == null)
+                finished = !finished;
+                Color targetColor = finished ? Color.green : Color.white;
+                foreach (RectTransform caracter in chars)
                 {
-                    instance = StartCoroutine(Move(0));
+                    caracter.GetComponent<TextMeshProUGUI>().color = targetColor;
                 }
             }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (!finished)
             {
-                selectedChar = (selectedChar + 1) % characters.Length;
-                chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
-                if (instance == null)
+                transform.GetChild(0).gameObject.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
-                    instance = StartCoroutine(Move(1));
+                    index = (index + 1) % chars.Length;
+                    childRect.anchoredPosition = chars[index].anchoredPosition;
+                    selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
                 }
+
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    index = (index - 1 + chars.Length) % chars.Length;
+                    childRect.anchoredPosition = chars[index].anchoredPosition;
+                    selectedChar = System.Array.IndexOf(characters, chars[index].GetComponent<TextMeshProUGUI>().text[0]);
+                }
+
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    selectedChar = (selectedChar - 1 + characters.Length) % characters.Length;
+                    chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
+                    if (instance == null)
+                    {
+                        instance = StartCoroutine(Move(0));
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    selectedChar = (selectedChar + 1) % characters.Length;
+                    chars[index].GetComponent<TextMeshProUGUI>().text = characters[selectedChar].ToString();
+                    if (instance == null)
+                    {
+                        instance = StartCoroutine(Move(1));
+                    }
+                }
+            }
+            else
+            {
+                transform.GetChild(0).gameObject.SetActive(false);
             }
         }
-
     }
 
     IEnumerator Move(int way)
--- Assets/MENU/Scripts/NameInputManager.cs
+++ Assets/MENU/Scripts/NameInputManager.cs
@@ -8,18 +8,41 @@ public class NameInputManager : MonoBehaviour
     public RectTransform player1;
     public RectTransform player2;
     public bool multiplayer;
+    public Image joinButton;
+    float timer = 0;
+    public Coroutine instance;
 
     void Update()
     {
         if (multiplayer)
         {
-            player1.anchoredPosition = new Vector2(-280, -80);
-            player2.gameObject.SetActive(true);
+            if (instance == null)
+            {
+                player1.anchoredPosition = new Vector2(-280, -80);
+                player2.gameObject.SetActive(true);
+                joinButton.color = new Color(1, 1, 1, 0);
+                if (player1.GetComponent<NameController>().finished && player2.GetComponent<NameController>().finished)
+                {
+                    instance = StartCoroutine(Finish());
+                }
+            }
         }
         else
         {
-            player1.anchoredPosition = new Vector2(0, -80);
-            player2.gameObject.SetActive(false);
+            if (instance == null)
+            {
+                joinButton.color = new Color(joinButton.color.r, joinButton.color.g, joinButton.color.b, Mathf.PingPong(Time.time * 1.5f, 1.0f));
+                player1.anchoredPosition = new Vector2(0, -80);
+                player2.gameObject.SetActive(false);
+                if (Input.GetButtonDown("A2") || Input.GetButtonDown("B2") || Input.GetButtonDown("C2")) multiplayer = true;
+            }
         }
+    }
+
+    IEnumerator Finish()
+    {
+        Debug.Log("empezo");
+        yield return new WaitForSeconds(10);
+        Debug.Log("temino");
     }
 }
