bb36b8e19907de409582f34067e37b971d243b2f
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-11-01 14:28:03 -0300

    UI

--- Assets/FentFighter/Scripts/Points_FF.cs
+++ Assets/FentFighter/Scripts/Points_FF.cs
@@ -12,24 +12,28 @@ public class Points_FF : MonoBehaviour
     public TextMeshProUGUI txtPlayer1Name;
     public TextMeshProUGUI txtPlayer2Name;
     public TextMeshProUGUI txtWinner;
-    public string player1Name;
-    public string player2Name;
+    int player1Score;
+    int player2Score;
     // Start is called before the first frame update
     public void UpdateScore(float player1Score, float player2Score)
     {
+        this.player1Score = (int)player1Score;
+        this.player2Score = (int)player2Score;
         txtPlayer1Score.text = player1Score.ToString();
         txtPlayer2Score.text = player2Score.ToString();
-        txtPlayer2Name.text = player1Name;
-        txtPlayer2Name.text = player2Name;
+        txtPlayer2Name.text = GameData.name1;
+        txtPlayer2Name.text = GameData.name2;
         if (player1Score > player2Score)
-            txtWinner.text = player1Name;
+            txtWinner.text = GameData.name1;
         else
-            txtWinner.text = player2Name;
+            txtWinner.text = GameData.name2;
     }
 
-    public void Menu()
+    public void Next()
     {
-        SceneManager.LoadScene("MENU");
+        ScoreboardManager.SaveNewScore(GameData.name1, player1Score, "FF");
+        ScoreboardManager.SaveNewScore(GameData.name2, player2Score, "FF");
+        SceneManager.LoadScene("Leaderboard");
     }
 
     public void PlayAgain()
