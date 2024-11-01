b281d741b5dc2327e758630b3f00dc936d78323a
AlanPessaj <alanpessaj123@gmail.com>
2024-11-01 10:34:39 -0300

    muchas csas

--- Assets/Fentiger/Scripts/EndManager_FG.cs
+++ Assets/Fentiger/Scripts/EndManager_FG.cs
@@ -20,14 +20,21 @@ public class EndManager_FG : MonoBehaviour
     {
         player1Canvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = player1Name;
         player1Canvas.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "SCORE: " + player1Score.ToString();
-        player2Canvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = player2Name;
-        player2Canvas.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "SCORE: " + player2Score.ToString();
+        //ScoreboardManager.SaveNewScore(player1Name, (int)player1Score, "FG");
+        if (player2Name != null)
+        {
+            //ScoreboardManager.SaveNewScore(player2Name, (int)player2Score, "FG");
+            player2Canvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = player2Name;
+            player2Canvas.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "SCORE: " + player2Score.ToString();
+        }
     }
 
-    public void Menu()
+    public void Next()
     {
-        SceneManager.LoadScene("MENU");
+        GameData.game = "FG";
+        SceneManager.LoadScene("Leaderboard");
     }
+
 
     public void PlayAgain()
     {
