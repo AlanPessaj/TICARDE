44dbe46d2c5ae1335884076fa118d13c94fd3b83
AlanPessaj <alanpessaj123@gmail.com>
2024-11-04 23:54:06 -0300

    ghost player desaparece al revivir

--- Assets/Fentiger/Scripts/Reanimator_FG.cs
+++ Assets/Fentiger/Scripts/Reanimator_FG.cs
@@ -15,12 +15,13 @@ public class Reanimator_FG : MonoBehaviour
                     GetComponent<Generator_FG>().players[1].transform.GetChild(0).gameObject.SetActive(false);
                     GetComponent<Generator_FG>().players[0].SetActive(true);
                     GetComponent<Generator_FG>().players[0].transform.position = GetComponent<Generator_FG>().players[1].transform.position;
+                    foreach (DieScript_FG ghost in GameObject.FindObjectsOfType<DieScript_FG>())
+                    {
+                        if (ghost.playerGhost) Destroy(ghost.gameObject);
+                    }
                 }
             }
-        }
 
-        if (GetComponent<Generator_FG>().initialMultiplayer)
-        {
             if (Input.GetButtonDown("A2") || Input.GetButtonDown("B2") || Input.GetButtonDown("C2") && !GetComponent<Generator_FG>().multiplayer & !GetComponent<Generator_FG>().isTherePlayer2)
             {
                 if (GetComponent<Generator_FG>().players[0].transform.GetChild(0).gameObject.activeSelf)
@@ -28,6 +29,10 @@ public class Reanimator_FG : MonoBehaviour
                     GetComponent<Generator_FG>().players[0].transform.GetChild(0).gameObject.SetActive(false);
                     GetComponent<Generator_FG>().players[1].SetActive(true);
                     GetComponent<Generator_FG>().players[1].transform.position = GetComponent<Generator_FG>().players[0].transform.position;
+                    foreach (DieScript_FG ghost in GameObject.FindObjectsOfType<DieScript_FG>())
+                    {
+                        if (ghost.playerGhost) Destroy(ghost.gameObject);
+                    }
                 }
             }
         }
