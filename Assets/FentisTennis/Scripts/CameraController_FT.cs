1612010a0941f57580c7c0a17d9e83a08cd1c402
AlanPessaj <alanpessaj123@gmail.com>
2024-11-08 12:10:32 -0300

    cambio de lado casi hecho

--- Assets/FentisTennis/Scripts/CameraController_FT.cs
+++ Assets/FentisTennis/Scripts/CameraController_FT.cs
@@ -9,45 +9,28 @@ public class CameraController_FT : MonoBehaviour
     new public Camera camera;
     Vector3 initialPosition;
     public Transform ball;
+    public bool resetPos;
     Vector3 oldBall;
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        initialPosition = camera.transform.position;
-        /*averagePosition = (player1.transform.position - player2.transform.position) / 2;
-        initAveragePosition = averagePosition;*/
-
+        initialPosition = camera.transform.localPosition;
     }
-    // Update is called once per frame
+
     void Update()
     {
         if (ball.position.y > -2)
         {
-            if (Vector3.Distance(initialPosition, camera.transform.position) > 20)
+            if (Vector3.Distance(initialPosition, camera.transform.localPosition) > 20)
             {
-                camera.transform.position = initialPosition;
+                camera.transform.localPosition = initialPosition;
             }
-            camera.transform.position += (ball.position - oldBall) / 10;
+            camera.transform.localPosition += (ball.position - oldBall) / 10;
             oldBall = ball.position;
+            if (resetPos)
+            {
+                camera.transform.localPosition = initialPosition; 
+                resetPos = false;
+            }
         }
-    }
-
-    void melavoyaguradarporlasdudas()
-    {
-        /*averagePosition = (player1.transform.position + player2.transform.position) / 2;
-        if (Vector3.Distance(initialPosition, camera.transform.position) > 2)
-        {
-            camera.transform.position = initialPosition;
-            counter = 20;
-        }
-        if (counter >= 9)
-        {
-            counter = 0;
-        }
-        //camera.transform.Translate(offset/10, Space.Self);
-        Vector3 finalPosition = averagePosition + initialPosition;
-        camera.transform.position = new Vector3(finalPosition.x, finalPosition.y, Mathf.Clamp(finalPosition.z, -21, 21));
-        counter++;
-        camera.fieldOfView = Mathf.Lerp(50, 60, Time.timeScale);*/
     }
 }
