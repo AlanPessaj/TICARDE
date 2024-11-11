4190df4b2b232d832d3158144ee08c08cd180acf
AlanPessaj <alanpessaj123@gmail.com>
2024-11-11 20:51:04 -0300

    end ui ft

--- Assets/FentisTennis/Scripts/Points_FT.cs
+++ Assets/FentisTennis/Scripts/Points_FT.cs
@@ -9,18 +9,14 @@ public class Points_FT : MonoBehaviour
 {
     public TextMeshProUGUI txtPlayer1Score;
     public TextMeshProUGUI txtPlayer2Score;
-    public TextMeshProUGUI txtPlayer1Name;
-    public TextMeshProUGUI txtPlayer2Name;
     public TextMeshProUGUI txtWinner;
     // Start is called before the first frame update
     private void Start()
     {
         GAMEMANAGER.Instance.GetComponent<conexion>().SendMessagestoArduino("0", new string[]{ "" });
-        txtPlayer1Score.text = GameData.score1.ToString();
-        txtPlayer2Score.text = GameData.score2.ToString();
-        txtPlayer1Name.text = GameData.name1;
-        txtPlayer2Name.text = GameData.name2;
-         txtWinner.text = GameData.p1Winner ? GameData.name1 : GameData.name2;
+        txtPlayer1Score.text = GameData.name1 + " " + GameData.score1.ToString();
+        txtPlayer2Score.text = GameData.name2 + " " + GameData.score2.ToString();
+        txtWinner.text = (GameData.p1Winner ? GameData.name1 : GameData.name2) + " WINS!";
         ScoreboardManager.SaveNewScore(GameData.name1, GameData.score1, "FT");
         ScoreboardManager.SaveNewScore(GameData.name2, GameData.score2, "FT");
     }
