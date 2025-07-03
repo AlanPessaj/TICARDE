f2d70a652ac4ce41a63cbbc3341b73f65bc258a5
AlanPessaj <alanpessaj123@gmail.com>
2025-07-03 15:10:26 -0300

    Update Generator_FG.cs

--- Assets/Fentiger/Scripts/Generator_FG.cs
+++ Assets/Fentiger/Scripts/Generator_FG.cs
@@ -95,8 +95,8 @@ public class Generator_FG : MonoBehaviour
             player1Alive = false;
         }
 
-        /*if (Input.GetKeyDown(KeyCode.Space))
-        {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
             //Portal
             //Instantiate(specials[2], new Vector3((int)camara.position.x + 25.5f, -1.5f, Random.Range(-12, 13)), Quaternion.Euler(0, 0, 90));
 
@@ -121,7 +121,7 @@ public class Generator_FG : MonoBehaviour
             {
                 Instantiate(specials[0], new Vector3(players[1].transform.position.x, 3, 18), Quaternion.identity);
             }*/
-        }*/
+        //}
 
         float difficultyPosition = 0; //reemplazo de camara.position.x
         if (difficulty >= 10 && difficulty % 10 == 0)
